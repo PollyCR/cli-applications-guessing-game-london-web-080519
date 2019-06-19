f83a7930269bae6375f00ebe08f4b4d69c447fc1
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-19 18:29:05 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -10,10 +10,6 @@ def run_guessing_game
     else
       puts "The computer guessed #{number}."
     end
-
-    puts "Guess a number between 1 and 6.\n"
-    number = rand(1..6).to_s
-    input = gets.chomp
   end
 
   puts "Goodbye!"
