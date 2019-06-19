9c0ce33b41934f67af29f4062e62e2e0d056f633
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-19 18:35:44 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 def run_guessing_game
   puts "Guess a number between 1 and 6."
   user_input = gets.chomp
-  number = rand(1..6) + 1.to_s 
+  number = (rand(1..6) + 1).to_s
   if user_input == "exit"
     puts "Goodbye!"
     elsif user_input == number 
